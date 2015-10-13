08e6fe9b278b7b3359299735826b432237ec704f
LiTingchang <tingchanglee@gmail.com>
2015-10-14 00:44:22 +0800

    登录注册验证流程

---
+++ app/src/main/java/com/yeah/android/activity/user/RestPasswordActivity.java
@@ -0,0 +1,18 @@
+package com.yeah.android.activity.user;
+
+import android.os.Bundle;
+
+import com.yeah.android.activity.BaseActivity;
+
+/**
+ * Created by litingchang on 15-10-13.
+ */
+public class RestPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
--- app/src/main/java/com/yeah/android/activity/user/SplashActivity.java
+++ app/src/main/java/com/yeah/android/activity/user/SplashActivity.java
@@ -1,13 +1,27 @@
 package com.yeah.android.activity.user;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.yeah.android.utils.UserInfoManager;
-import com.yeah.android.activity.MainActivity;
-import com.yeah.android.activity.BaseActivity;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.yeah.android.R;
+import com.yeah.android.activity.BaseActivity;
+import com.yeah.android.activity.MainActivity;
+import com.yeah.android.utils.UserInfoManager;
+import com.yeah.android.view.LoopViewPager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -18,47 +32,96 @@ import butterknife.OnClick;
  */
 public class SplashActivity extends BaseActivity {
 
-    @InjectView(R.id.splash_login)
-    LinearLayout btnLogin;
-    @InjectView(R.id.splash_bind)
-    LinearLayout btnBind;
-    @InjectView(R.id.splash_regist)
-    LinearLayout btnRegist;
-    @InjectView(R.id.splash_action_layout)
-    LinearLayout splashActionLayout;
+
+    @InjectView(R.id.splash_next)
+    ImageView splashNext;
+    @InjectView(R.id.splash_arrow_up)
+    ImageView splashArrowUp;
+    @InjectView(R.id.splash_pager)
+    LoopViewPager splashPager;
+
+    private int tickCount = 0;
+    private static final int [] imgs = {R.drawable.splash_p1, R.drawable.splash_p2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         ButterKnife.inject(this);
+
+
+        splashPager.setAdapter(new RecommendPagerAdapter(SplashActivity.this, imgs));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        new CountDownTimer(30000000, 1500) {
+            public void onTick(long millisUntilFinished) {
+                tickCount++;
+                splashPager.setCurrentItem(tickCount % imgs.length);
+            }
+            public void onFinish() {
+                doNext();
+            }
+        }.start();
+    }
 
+    @OnClick(R.id.splash_next)
+    public void login() {
+        doNext();
+    }
+
+    private void doNext() {
         if (UserInfoManager.isLogin()) {
             MainActivity.launch(SplashActivity.this);
-            SplashActivity.this.finish();
         } else {
-            splashActionLayout.setVisibility(View.VISIBLE);
+            LoginActivity.launch(SplashActivity.this);
         }
+        SplashActivity.this.finish();
     }
 
-    @OnClick(R.id.splash_login)
-    public void login() {
+    class RecommendPagerAdapter extends PagerAdapter {
 
-        LoginActivity.launch(SplashActivity.this);
-    }
+        private Context mContext;
+        private int [] mImgs;
 
-    @OnClick(R.id.splash_bind)
-    public void bind() {
-        BindActivity.launch(SplashActivity.this);
-    }
 
-    @OnClick(R.id.splash_regist)
-    public void regist() {
-        RegistActivity.launch(SplashActivity.this);
+        public RecommendPagerAdapter(Context context, int [] imgs) {
+            mContext = context;
+            mImgs = imgs;
+        }
+
+
+        @Override
+        public int getCount() {
+            return mImgs.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object o) {
+            return view == o;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+
+            if(mImgs.length == 0 || position >= mImgs.length) {
+                return null;
+            }
+
+            View view = LayoutInflater.from(mContext).inflate(R.layout.view_splash_item, null);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.splash_pager_view);
+            imageView.setImageResource(mImgs[position]);
+
+            container.addView(view);
+            return view;
+        }
     }
 }
